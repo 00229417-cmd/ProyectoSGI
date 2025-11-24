0c813778c205660fd17442fe9fec61e8ad4e6b4f
00229417-cmd <00229417@uca.edu.sv>
2025-11-24 08:53:14 -0600

    Update __init__.py

--- modulos/db/__init__.py
+++ modulos/db/__init__.py
@@ -1,1 +1,18 @@
+# modulos/db/__init__.py
+from . import crud_miembros
+from . import crud_ahorro
+from . import crud_aporte
+from . import crud_prestamo
+from . import crud_cuota
+from . import crud_caja
+from . import crud_reunion
+from . import crud_asistencia
+from . import crud_multa
+from . import crud_cierre
+from . import crud_promotora
+from . import crud_grupo
+from . import crud_ciclo
+from . import crud_administrador   # <= asegurarse que exista este archivo
+from . import crud_reporte
+# etc...
 # paquete modulos.db
